ed2ec6b51fe0e2600ae7ca1262b3a3ff51b4544e
DESKTOP-15QCO22\Amit <amitkuliyal@gmail.com>
2018-02-26 18:16:36 +0530

    Changes till the EOD

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
+
+public class ShipMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    AudioSource audioSource;
+
+
+    public AudioClip engineAudioClip;
+    public float rotationSpeed = 10f;
+    public float thrustSpeed = 100f;
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = engineAudioClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInputs();
+        Rotate();
+    }
+
+
+    void PlayerInputs()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+
+            }
+
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+
+    }
+
+
+    void Rotate()
+    {
+        rb.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+        }
+
+        rb.freezeRotation = false;
+
+    }
+}
