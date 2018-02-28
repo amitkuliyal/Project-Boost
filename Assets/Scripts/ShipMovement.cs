9454147fefe1113a5c4214617510e8348eef25e2
DESKTOP-15QCO22\Amit <amitkuliyal@gmail.com>
2018-02-28 17:41:36 +0530

    28 Feb EOD Build

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -1,95 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(AudioSource))]
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (AudioSource))]
 
-public class ShipMovement : MonoBehaviour
-{
-    Rigidbody rb;
-    AudioSource audioSource;
+public class ShipMovement : MonoBehaviour {
+    public enum ShipState { Flying, Dying };
+ public ShipState currentState;
+ Rigidbody rb;
+ AudioSource audioSource;
 
+ private AudioClip engineAudioClip;
+ [SerializeField] float rotationSpeed = 10f;
+ [SerializeField] float thrustSpeed = 1000f;
 
-    private AudioClip engineAudioClip;
-    [SerializeField] float rotationSpeed = 10f;
-    [SerializeField] float thrustSpeed = 1000f;
+ public AudioClip EngineAudioClip;
 
-    public AudioClip EngineAudioClip;
-
-
-    // Use this for initialization
-    void Start()
-    {
-
-        rb = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = EngineAudioClip;
+ // Use this for initialization
+ void Start () {
+ currentState = ShipState.Flying;
+ rb = GetComponent<Rigidbody> ();
+ audioSource = GetComponent<AudioSource> ();
+ audioSource.clip = EngineAudioClip;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        PlayerInputs();
-        Rotate();
+    void Update () {
+        if (!(currentState == ShipState.Dying)) {
+            PlayerInputs ();
+            Rotate ();
+        }
     }
 
+    void PlayerInputs () {
 
-    void PlayerInputs()
-    {
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
+        if (Input.GetKey (KeyCode.Space)) {
+            rb.AddRelativeForce (Vector3.up * thrustSpeed * Time.deltaTime);
+            if (!audioSource.isPlaying) {
+                audioSource.Play ();
 
             }
 
+        } else {
+            audioSource.Stop ();
         }
-        else
-        {
-            audioSource.Stop();
-        }
-
 
     }
 
-
-    void Rotate()
-    {
+    void Rotate () {
         rb.freezeRotation = true;
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+        if (Input.GetKey (KeyCode.A)) {
+            transform.Rotate (Vector3.forward * rotationSpeed * Time.deltaTime);
+        } else if (Input.GetKey (KeyCode.D)) {
+            transform.Rotate (Vector3.back * rotationSpeed * Time.deltaTime);
         }
 
         rb.freezeRotation = false;
 
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        switch (collision.gameObject.tag)
-        {
-            case "Danger":
+    void OnCollisionEnter (Collision collision) {
+        switch (collision.gameObject.tag) {
+            case "Obstacle":
                 {
-                    print("Take Damage");
+                    currentState = ShipState.Dying;
+                    SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
                     break;
                 }
 
-
-
-
+            case "Fuel":
+                {
+                    print ("Fuel Gathered");
+                    break;
+                }
 
             default:
                 {
-                    print("It's Okay");
+                    currentState = ShipState.Flying;
                     break;
                 }
         }
